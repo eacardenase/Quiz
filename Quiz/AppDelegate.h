6c48600e290a83c815e1310e0aac56ef7e95b2d6
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-27 16:50:19 -0500

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Edwin Cardenas on 27/01/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Quiz/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Quiz
+//
+//  Created by Edwin Cardenas on 27/01/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
