8e2538ec2db80310abb3f2f270e82ba8aa011885
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-28 11:00:26 -0500

    feat: show questions and answers dynamically based on user interaction

--- Quiz/ViewController.h
+++ Quiz/ViewController.h
@@ -9,11 +9,18 @@
 
 @interface ViewController : UIViewController
 
-@property (nonatomic) UILabel *questionField;
-@property (nonatomic) UILabel *answerField;
+@property (nonatomic) UILabel *questionLabel;
+@property (nonatomic) UILabel *answerLabel;
 
 @property (nonatomic) UIButton *showQuestionButton;
 @property (nonatomic) UIButton *showAnswerButton;
+
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+- (void)showQuestion:(id)sender;
+- (void)showAnswer:(id)sender;
 
 @end
 
--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -13,60 +13,82 @@
 
 @implementation ViewController
 
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self) {
+        self.questions = @[
+            @"From what is cognac made?",
+            @"What is 7+7?",
+            @"What is the capital of Vermont?",
+        ];
+        self.answers = @[
+            @"Grapes",
+            @"14",
+            @"Montpelier",
+        ];
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self setupViews];
+    NSLog(@"%@", self.questions);
+    NSLog(@"%@", self.answers);
 }
 
 - (void)setupViews
 {
-    self.questionField = [UILabel new];
-    self.questionField.text = @"";
-    self.questionField.textAlignment = NSTextAlignmentCenter;
+    self.questionLabel = [UILabel new];
+    self.questionLabel.text = @"";
+    self.questionLabel.textAlignment = NSTextAlignmentCenter;
     
     self.showQuestionButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.showQuestionButton setTitle:@"Show Question" forState:UIControlStateNormal];
     self.showQuestionButton.backgroundColor = [UIColor systemGray5Color];
     [self.showQuestionButton addTarget:self action:@selector(showQuestion:) forControlEvents:UIControlEventTouchUpInside];
     
-    self.answerField = [UILabel new];
-    self.answerField.text = @"???";
-    self.answerField.textAlignment = NSTextAlignmentCenter;
+    self.answerLabel = [UILabel new];
+    self.answerLabel.text = @"???";
+    self.answerLabel.textAlignment = NSTextAlignmentCenter;
     
     self.showAnswerButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.showAnswerButton setTitle:@"Show Answer" forState:UIControlStateNormal];
     self.showAnswerButton.backgroundColor = [UIColor systemGray5Color];
     [self.showAnswerButton addTarget:self action:@selector(showAnswer:) forControlEvents:UIControlEventTouchUpInside];
     
-    self.questionField.translatesAutoresizingMaskIntoConstraints = NO;
+    self.questionLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.showQuestionButton.translatesAutoresizingMaskIntoConstraints = NO;
-    self.answerField.translatesAutoresizingMaskIntoConstraints = NO;
+    self.answerLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.showAnswerButton.translatesAutoresizingMaskIntoConstraints = NO;
     
-    [self.view addSubview:self.questionField];
+    [self.view addSubview:self.questionLabel];
     [self.view addSubview:self.showQuestionButton];
-    [self.view addSubview:self.answerField];
+    [self.view addSubview:self.answerLabel];
     [self.view addSubview:self.showAnswerButton];
     
     [NSLayoutConstraint activateConstraints:@[
-        [self.questionField.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20],
-        [self.questionField.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
-        [self.questionField.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20],
+        [self.questionLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20],
+        [self.questionLabel.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
+        [self.questionLabel.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20],
         
-        [self.showQuestionButton.topAnchor constraintEqualToAnchor:self.questionField.bottomAnchor constant:20],
-        [self.showQuestionButton.leadingAnchor constraintEqualToAnchor:self.questionField.leadingAnchor],
-        [self.showQuestionButton.trailingAnchor constraintEqualToAnchor:self.questionField.trailingAnchor],
+        [self.showQuestionButton.topAnchor constraintEqualToAnchor:self.questionLabel.bottomAnchor constant:20],
+        [self.showQuestionButton.leadingAnchor constraintEqualToAnchor:self.questionLabel.leadingAnchor],
+        [self.showQuestionButton.trailingAnchor constraintEqualToAnchor:self.questionLabel.trailingAnchor],
         
-        [self.answerField.topAnchor constraintEqualToAnchor:self.showQuestionButton.bottomAnchor constant:250],
-        [self.answerField.leadingAnchor constraintEqualToAnchor:self.questionField.leadingAnchor],
-        [self.answerField.trailingAnchor constraintEqualToAnchor:self.questionField.trailingAnchor],
+        [self.answerLabel.topAnchor constraintEqualToAnchor:self.showQuestionButton.bottomAnchor constant:250],
+        [self.answerLabel.leadingAnchor constraintEqualToAnchor:self.questionLabel.leadingAnchor],
+        [self.answerLabel.trailingAnchor constraintEqualToAnchor:self.questionLabel.trailingAnchor],
         
-        [self.showAnswerButton.topAnchor constraintEqualToAnchor:self.answerField.bottomAnchor constant:20],
-        [self.showAnswerButton.leadingAnchor constraintEqualToAnchor:self.questionField.leadingAnchor],
-        [self.showAnswerButton.trailingAnchor constraintEqualToAnchor:self.questionField.trailingAnchor],
+        [self.showAnswerButton.topAnchor constraintEqualToAnchor:self.answerLabel.bottomAnchor constant:20],
+        [self.showAnswerButton.leadingAnchor constraintEqualToAnchor:self.questionLabel.leadingAnchor],
+        [self.showAnswerButton.trailingAnchor constraintEqualToAnchor:self.questionLabel.trailingAnchor],
     ]];
 }
 
@@ -74,12 +96,23 @@
 
 - (void)showQuestion:(id)sender
 {
-    NSLog(@"showQuestion tapped.");
+    self.currentQuestionIndex++;
+    
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
 }
 
 - (void)showAnswer:(id)sender
 {
-    NSLog(@"showAnswer tapped.");
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    
+    self.answerLabel.text = answer;
 }
 
 @end
